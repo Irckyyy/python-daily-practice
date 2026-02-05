2d3374b28503a3a694bbbd1f19660f012ab28554
Irckyyy <ericsamillano23@gmail.com>
2026-02-06 01:59:14 +0800

    feat: add division operation

--- daily_challenges/day03_mini_CLI_calculator.py
+++ daily_challenges/day03_mini_CLI_calculator.py
@@ -6,8 +6,8 @@ import operator
 def get_operation():
     while True:
         
-        operand = input("Please choose the operation(+, -, *, /, =): ").strip()
-        if operand not in ["+", "-", "*", "/", "="]: 
+        operand = input("Please choose the operation(+, -, *, /): ").strip()
+        if operand not in ["+", "-", "*", "/"]: 
             print("Please enter a correct operation.")
             continue
         
@@ -23,7 +23,8 @@ def get_numbers_until_equals():
         try: 
             num = float(val)  
             numbers.append(num)
-        except ValueError: print("Invalid input. Please enter a valid integer.") 
+        except ValueError: 
+            print("Invalid input. Please enter a valid number.") 
         continue
     return numbers
     
@@ -34,20 +35,40 @@ def compute(operand, numbers):
         result = reduce(operator.sub, numbers)  
     elif operand == "*":
         result = math.prod(numbers)
+    elif operand == "/":
+        try:
+            result = reduce(operator.truediv, numbers)
+        except ZeroDivisionError:
+            return "Error: Cannot divide by zero. Please try again."
 
-    if result == int(result): 
-        result = int(result)
-        return result
-    else:
-        return result
+
+    if isinstance(result, (int, float)) and result.is_integer():
+        return int(result)
+    return result
 
 def main():
 
-    print("------------------------------\nHello there! Welcome to CLI Calculator tool that offer basic operations.\n------------------------------\n")
-    operand = get_operation()
-    numbers = get_numbers_until_equals()
-    result = compute(operand, numbers)
-    print(result)
+    print("------------------------------\nHello there! Welcome to CLI Calculator tool that offers basic operations.\n------------------------------\n")
+    
+    while True:
+        operand = get_operation()
+        numbers = get_numbers_until_equals()
+        result = compute(operand, numbers)
+    
+        clean_numbers = [int(n) if n.is_integer() else n for n in numbers]
+        print(f"\nNumbers: {clean_numbers}")
+        print(f"Operation: '{operand}'")
+        print(f"Result: {result}\n\n")
+    
+        again = input("Do you want to try again? (y/n): ").lower().strip()
+        if again == "y":
+            continue
+        elif again == "n":
+            print("Thank you for using the program!")
+            break
+        else:
+            print("Please enter 'y' or 'n'.")
+            continue
     
 
 if __name__ == "__main__":

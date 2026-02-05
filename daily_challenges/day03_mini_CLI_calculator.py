8be5386cdf7ece954c1cfdd9b19927c45aff834b
Irckyyy <ericsamillano23@gmail.com>
2026-02-05 23:30:58 +0800

    Bit of second function

--- daily_challenges/day03_mini_CLI_calculator.py
+++ daily_challenges/day03_mini_CLI_calculator.py
@@ -3,18 +3,34 @@
 def get_operation():
     while True:
         
-        operand = input("Please choose the operation(+, -, *, /): ").strip()
-        if operand not in ["+", "-", "*", "/"]: 
-            print("Please enter a correct operation")
+        operand = input("Please choose the operation(+, -, *, /, =): ").strip()
+        if operand not in ["+", "-", "*", "/", "="]: 
+            print("Please enter a correct operation.")
             continue
         
         return operand
 
+def get_numbers_until_equals():
+        
+    numbers = []
+    while True:
+        val = input("Enter an element (or press Enter to finish): ")
+        if val == "":
+            break  
+        try: 
+            num = float(val)  
+            numbers.append(num)
+        except ValueError: print("Invalid input. Please enter a valid integer.") 
+        continue
+    
+    print(numbers)
 
 def main():
 
     print("------------------------------\nHello there! Welcome to CLI Calculator tool that offer basic operations.\n------------------------------\n")
     get_operation()
+    get_numbers_until_equals()
+    
 
 
         

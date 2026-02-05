0608b3186bb1a78ad72d358f7ceaff1df51296bf
Irckyyy <ericsamillano23@gmail.com>
2026-02-06 01:05:09 +0800

    feat: add addition operation

--- daily_challenges/day03_mini_CLI_calculator.py
+++ daily_challenges/day03_mini_CLI_calculator.py
@@ -1,4 +1,7 @@
 # day03_mini_CLI_calculator
+import math
+from functools import reduce
+import operator
 
 def get_operation():
     while True:
@@ -22,25 +25,30 @@ def get_numbers_until_equals():
             numbers.append(num)
         except ValueError: print("Invalid input. Please enter a valid integer.") 
         continue
+    return numbers
     
-    print(numbers)
+def compute(operand, numbers):
+    if operand == "+":
+        result = sum(numbers)
+    elif operand == "-":
+        result = reduce(operator.sub, numbers)  
+    elif operand == "*":
+        result = math.prod(numbers)
+
+    if result == int(result): 
+        result = int(result)
+        return result
+    else:
+        return result
 
 def main():
 
     print("------------------------------\nHello there! Welcome to CLI Calculator tool that offer basic operations.\n------------------------------\n")
-    get_operation()
-    get_numbers_until_equals()
+    operand = get_operation()
+    numbers = get_numbers_until_equals()
+    result = compute(operand, numbers)
+    print(result)
     
-
-
-        
-
-
-while True:
-    
-    
-    
-    break
 
 if __name__ == "__main__":
     main()
